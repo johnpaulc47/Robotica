8f08589acb326c93aab7145de599339158ef6d78
Johnpaul Chiemerie <johnpaulcc47@gmail.com>
2018-09-11 17:11:27 -0300

    modifiquei o arquivo reobo_desenhista

---
+++ Robo_desenhista/Robo_desenhista.ino
@@ -0,0 +1,12 @@
+/**
+ * Robo Desenhinsta
+ */@author Johnpaul chiemerie
+ *Data 11/09/2018
+ /*
+  * 
+  * 
+  */pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    
